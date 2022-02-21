4d199523b4f2df1da9e3d865a560c8e4261fddfc
alvarolozrod <alvarloz@ucm.es>
2022-02-21 23:22:12 +0100

    Cambios en los scripts de Player_Life_Component y Damage
    He optimizado el programa de rebote, haciendolo más generico para que sirva para todos los enemigos.
    
    He eliminado el metodo rebote y he pasado su contenido al de Player_Life_Component

--- 2025/Assets/Scripts/Damage.cs
+++ 2025/Assets/Scripts/Damage.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damage_Barricada : MonoBehaviour
+public class Damage : MonoBehaviour
 {
     [SerializeField]
     private Player_Life_Component _player_life_component;
 
+    float posXenemy, posYenemy;
+
+    private Transform _myTransform;
 
     void Main()
     {
         _player_life_component = GetComponent<Player_Life_Component>();
+        
     }
 
     // Update is called once per frame
@@ -19,9 +23,19 @@ public class Damage_Barricada : MonoBehaviour
 
     }
 
+    public void guardarPosEnemy()
+    {
+        posXenemy = _myTransform.position.x;
+        posYenemy = _myTransform.position.y;
+
+
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        _player_life_component.Damage();
+        _player_life_component.Damage(10);
+        _player_life_component.Rebote(posXenemy, posYenemy);
     }
 }
 
--- 2025/Assets/Scripts/Player_Life_Component.cs
+++ 2025/Assets/Scripts/Player_Life_Component.cs
@@ -7,11 +7,18 @@ public class Player_Life_Component : MonoBehaviour
 
     int curretLife = 100;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    float posXplayer, posYplayer;
 
-    }
+
+
+    private Transform _myTransform;
+
+    // Start is called before the first frame update
+     void Start()
+     {
+        _myTransform = transform;
+
+     }
 
     // Update is called once per frame
     void Update()
@@ -19,8 +26,51 @@ public class Player_Life_Component : MonoBehaviour
 
     }
 
-    public void Damage()
+    public void Damage(int damage)
     {
-        curretLife -= 10;
+        curretLife -= damage;
     }
+
+    private void guardarPosPlayer()
+    {
+        posXplayer = _myTransform.position.x;
+        posYplayer = _myTransform.position.y;
+
+
+    }
+
+    public void Rebote( float posXenemy, float posYenemy)
+    {
+        if (posXplayer < posXenemy)
+        {
+            _myTransform.position = new Vector2(posXplayer - 1, posYplayer);
+        }
+        else if (posXplayer > posXenemy)
+        {
+            _myTransform.position = new Vector2(posXplayer + 1, posYplayer);
+        }
+        else if (posYplayer < posYenemy)
+        {
+            _myTransform.position = new Vector2(posXplayer , posYplayer-1);
+        }
+        else if (posYplayer > posYenemy)
+        {
+            _myTransform.position = new Vector2(posXplayer , posYplayer+1);
+        }
+
+    }
+
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Damage>())
+        {
+
+            if (posXplayer < )
+                _myTransform.position = new Vector2(posXplayer, posYplayer);
+
+
+        }
+
+    }*/
 }
