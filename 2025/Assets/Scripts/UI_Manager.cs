fb41c6357ab4d643b56e9858e9cea4dcc8b316f3
Cao33 <pablocao@ucm.es>
2022-03-12 18:38:41 +0100

    textos ui prueba

--- 2025/Assets/Scripts/UI_Manager.cs
+++ 2025/Assets/Scripts/UI_Manager.cs
@@ -18,8 +18,16 @@ public class UI_Manager : MonoBehaviour
     #region parameters
     public Text balasg;
     public Text balassg;
+    public Text palancaaviso;
+    private float temporizadorpalanca;
+    private bool gogo=false;
     #endregion
     #region methods
+    public void palanca()
+    {
+        palancaaviso.text = "Pulse 1 para golpear con la palanca";
+        gogo = true;
+    }
     public void balagUI(int bala,int cargador)
     {
         balasg.text = "" + bala + "/" + cargador;
@@ -35,11 +43,21 @@ public class UI_Manager : MonoBehaviour
         _instance = this;
         balasg.text = "";
         balassg.text = "";
+        palancaaviso.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(gogo)
+        {
+            temporizadorpalanca += Time.deltaTime;
+            if (temporizadorpalanca >= 5)
+            {
+                palancaaviso.text = "";
+                temporizadorpalanca = 0;
+                gogo = false;
+            }
+        }
     }
 }
