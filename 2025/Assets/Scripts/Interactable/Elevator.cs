417570a39cc52a4deefaa5da5523fd123e026c63
PabloCerrada <75524849+PabloCerrada@users.noreply.github.com>
2022-03-28 02:16:29 +0200

    Animacion del ascensor cuajada

--- 2025/Assets/Scripts/Interactable/Elevator.cs
+++ 2025/Assets/Scripts/Interactable/Elevator.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    bool elevator = false;
+
+    #region references
     [SerializeField] private GameObject _changeLevel;
+    [SerializeField] private Animator _myAnimator;
+    //[SerializeField] private GameObject _elevator;
+    #endregion
     #region methods
     public void IsOpenElevator()
     {
@@ -14,8 +20,10 @@ public class Elevator : MonoBehaviour
     public void OpenElevator()
     {
         //GameManager.Instance.OpenElevatorDoor();
-        //Hacer animacion
+        elevator = true;
+        _myAnimator.SetBool("Elevator", elevator);
         _changeLevel.SetActive(true);
+        this.gameObject.SetActive(false);
     }
     #endregion
     // Start is called before the first frame update
