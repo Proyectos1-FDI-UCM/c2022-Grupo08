17451ca86f28391f58ab4016ef4197d7b29fc6f0
alvarolozrod <alvarloz@ucm.es>
2022-03-29 21:19:07 +0200

    Cambios escena

--- 2025/Assets/Scripts/Player/Player_Life_Component.cs
+++ 2025/Assets/Scripts/Player/Player_Life_Component.cs
@@ -116,7 +116,7 @@ public class Player_Life_Component : Life_Component
         //_myAnimator.SetInteger("VidaPlayer", _currentLife);
 
 
-       // _lifeBar.fillAmount = _currentLife / _maxLife;
+        _lifeBar.fillAmount = _currentLife / _maxLife;
 
         //Debug.Log(_currentLife);
         
