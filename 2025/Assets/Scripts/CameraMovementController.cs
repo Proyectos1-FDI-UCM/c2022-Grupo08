d13b2d8ac8b5e95ac274d89b7f25b1786c26105e
PabloCerrada <pcvatm@gmail.com>
2022-04-02 03:08:14 +0200

    Limites de camara arreglados en ambos niveles

--- 2025/Assets/Scripts/CameraMovementController.cs
+++ 2025/Assets/Scripts/CameraMovementController.cs
@@ -5,24 +5,14 @@ using UnityEngine;
 public class CameraMovementController : MonoBehaviour
 {
     #region parameters
-    [SerializeField]
-    private float _factorDeDelay = 1.0f; // Delay entre el movimiento del jugador y el movimiento de la cámara
-    [SerializeField]
-    private float _distanceToCenter = 1.0f; // Distancia del jugador al centro de la pantalla para dar mejor sensación
+    [SerializeField] private float _delayFactor = 1.0f; // Delay entre el movimiento del jugador y el movimiento de la cámara
     #endregion
 
     #region references
-    [SerializeField]
-    private Transform _leftLimit;
-    [SerializeField]
-    private Transform _rightLimit;
-    [SerializeField]
-    private Transform _topLimit;
-    [SerializeField]
-    private Transform _downLimit;
+    [SerializeField] private Transform _leftDownLimit;
+    [SerializeField] private Transform _rightTopLimit;
 
-    [SerializeField]
-    private GameObject _targetObject; // Objeto al que siga la camara (será el jugador)
+    [SerializeField] private GameObject _targetObject; // Objeto al que siga la camara (será el jugador)
     private Transform _targetObjectTransform;
     private Transform _myTransform;
     #endregion
@@ -32,12 +22,11 @@ public class CameraMovementController : MonoBehaviour
     {
         _myTransform = transform;
         _targetObjectTransform = _targetObject.transform;
-        _myTransform.position = new Vector3(_targetObjectTransform.position.x, _targetObjectTransform.position.y, _myTransform.position.z);
     }                                                                                               
 
     // Update is called once per frame
     void Update()
     {
-        _myTransform.position = Vector3.Lerp(_myTransform.position, new Vector3(Mathf.Clamp(_targetObjectTransform.position.x, _leftLimit.position.x, _rightLimit.position.x), Mathf.Clamp(_targetObjectTransform.position.y, _downLimit.position.y, _topLimit.position.y), _myTransform.position.z), Time.deltaTime * _factorDeDelay); // Interpolación lineal
+        _myTransform.position = Vector3.Lerp(_myTransform.position, new Vector3(Mathf.Clamp(_targetObjectTransform.position.x, _leftDownLimit.position.x, _rightTopLimit.position.x), Mathf.Clamp(_targetObjectTransform.position.y, _leftDownLimit.position.y, _rightTopLimit.position.y), _myTransform.position.z), Time.deltaTime * _delayFactor); // Interpolación lineal
     }
 }
